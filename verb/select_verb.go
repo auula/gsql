6065543ff956cb70020c2970c54daf294585491a
Dings <deen.job@qq.com>
2021-09-03 21:00:19 +0800

    updated: selecter verb.

--- verb/select_verb.go
+++ verb/select_verb.go
@@ -1,11 +1,34 @@
 package verb
 
+type SortType int8
+
+const (
+	DESC SortType = iota
+	ASC
+)
+
 // Selecter select statement trait
 // Exmaple:
 // gsql.Select(user.id,user.name).Where(user.id," > ?",10).From("").Build().
 // From("") is empty use model structured name.
 type Selecter interface {
+	Builder
+	Distinct() Selecter
 	Select() Selecter
 	Where() Selecter
 	From() Selecter
+}
+
+type Filter interface {
+	Builder
+	Limit(x int, y int) Filter
+	Order(field interface{}, sort SortType) Filter
+}
+
+type Alias interface {
+	As(field interface{}, asName string) string
+}
+
+func AS(field interface{}, asName string) string {
+	return ""
 }
