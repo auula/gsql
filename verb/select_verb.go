ebf9539abddd91e441ec5179fa3f2522a6e5c588
Dings <deen.job@qq.com>
2021-09-03 20:35:41 +0800

    add: builder trait.

--- verb/select_verb.go
+++ verb/select_verb.go
@@ -1,6 +1,9 @@
 package verb
 
 // Selecter select statement trait
+// Exmaple:
+// gsql.Select(user.id,user.name).Where(user.id," > ?",10).From("").Build().
+// From("") is empty use model structured name.
 type Selecter interface {
 	Select() Selecter
 	Where() Selecter
